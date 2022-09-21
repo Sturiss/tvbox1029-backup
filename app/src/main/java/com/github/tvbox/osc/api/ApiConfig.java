dadbf76c5550861450cd38804b9b1cb5aa7a830a
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-09-21 10:31:33 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,8 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://ghproxy.com/https://raw.githubusercontent.com/Sturiss/douban/main/app/src/main/assets/LionMovies/rcgold.json
+");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
