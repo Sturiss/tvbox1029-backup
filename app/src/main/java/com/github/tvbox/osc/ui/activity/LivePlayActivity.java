3ce4e2ed0113c61617a1ba1347b6cb5035dab6d1
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-09-23 15:36:52 +0800

    Update LivePlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -227,8 +227,7 @@ public class LivePlayActivity extends BaseActivity {
             mRightEpgList.setAdapter(myAdapter);
             mRightEpgList.setSelection(i);
         }else{
-          //  Epginfo epgbcinfo = new Epginfo("暂无节目信息", "00:00", "23:59");
-           Epginfo epgbcinfo = new Epginfo("生日钞——亿万分之一的心意", "超有 ", "心意 ");
+          Epginfo epgbcinfo = new Epginfo("自定义播放神器", "00:00", "23:59");
           arrayList.add(epgbcinfo);
             epgdata=arrayList;
             myAdapter = new MyEpgAdapter( epgdata,this,0);
@@ -281,7 +280,7 @@ public class LivePlayActivity extends BaseActivity {
             ((TextView) findViewById(R.id.tv_current_program_time)).setText("暂无信息");
             ((TextView) findViewById(R.id.tv_current_program_name)).setText("");
          //   ((TextView) findViewById(R.id.tv_next_program_time)).setText("开源测试软件,请勿商用以及播放违法内容!!!!");
-            ((TextView) findViewById(R.id.tv_next_program_time)).setText("专业礼品定制，贵金属、纪念钱币、生日钞礼品定制!!!!");
+            ((TextView) findViewById(R.id.tv_next_program_time)).setText("专业礼品定制，贵金属、纪念钱币、生日钞礼品定制!!");
             ((TextView) findViewById(R.id.tv_next_program_name)).setText("");
             if (hsEpg.containsKey(channel_Name.getChannelName())) {
                 ArrayList arrayList = (ArrayList) hsEpg.get(channel_Name.getChannelName());
