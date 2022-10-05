fe941af8bc2064bbcb7a2ac6f5ee741520785e3d
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-10-05 13:42:28 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -86,7 +86,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "asset://LionMovies/src.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
