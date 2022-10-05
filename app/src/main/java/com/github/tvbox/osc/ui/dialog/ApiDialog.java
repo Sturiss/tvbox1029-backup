200310a9dd108b04550ecdb3a16fc8d4d110f215
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-10-05 13:26:19 +0800

    Update ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -61,6 +61,8 @@ public class ApiDialog extends BaseDialog {
             @Override
             public void onClick(View v) {
                 String newApi = inputApi.getText().toString().trim();
+  // if (!newApi.isEmpty() && (newApi.startsWith("http") || newApi.startsWith("clan") || newApi.startsWith("asset"))) {
+
                 if (!newApi.isEmpty()) {
                     ArrayList<String> history = Hawk.get(HawkConfig.API_HISTORY, new ArrayList<String>());
                     if (!history.contains(newApi))
