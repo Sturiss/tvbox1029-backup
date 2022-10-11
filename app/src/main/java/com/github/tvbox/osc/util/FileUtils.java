8d295d419e9f21c9c2b55b62426bc73af085b1b7
Sturiss <106640910+Sturiss@users.noreply.github.com>
2022-10-11 10:08:42 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
@@ -3,13 +3,18 @@ package com.github.tvbox.osc.util;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 
 public class FileUtils {
 
@@ -68,6 +73,33 @@ public class FileUtils {
         }
         file.delete();
     }
+    
+    
+        
+    public static void appendFile(File file, String content) {
+            BufferedWriter out = null;
+            try {
+                out = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(file, true)));
+                out.write(content);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
 
     public static String readFileToString(String path, String charsetName) {
         // 定义返回结果
